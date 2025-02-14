c50739f25253cbe5b936a7ee55b31d1c8dbe791a
webgaurav02 <gauravcodes123@gmail.com>
2025-02-14 22:46:11 +0530

    Fix email subject

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -3,51 +3,6 @@ import handlebars from 'handlebars';
 import fs from 'fs';
 import path from 'path';
 
-export const sendEmail = async (to, subject, htmlContent, pdfBuffer, ticketId) => {
-
-    try {
-        // Create a transporter
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
-        let attachments = null;
-
-        if (ticketId) {
-            attachments = [
-                {
-                    filename: `ticket_${ticketId}.pdf`,
-                    content: pdfBuffer,
-                    contentType: 'application/pdf',
-                },
-            ]
-        }
-
-        if (!htmlContent) {
-            htmlContent = "A new submission has been received"
-        }
-
-        // Set email options
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            html: htmlContent,
-            attachments: attachments,
-        };
-
-        // Send email
-        const info = await transporter.sendMail(mailOptions);
-        console.log('Email sent: ' + info.response);
-    } catch (error) {
-        console.error('Error sending email:', error);
-    }
-};
-
 export const sendOtpEmail = async (email, emailHtml) => {
 
     try {
@@ -64,7 +19,7 @@ export const sendOtpEmail = async (email, emailHtml) => {
         const mailOptions = {
             from: process.env.EMAIL_USER,
             to: email,
-            subject: 'Minus01 Verification Code',
+            subject: 'MINUS01 Verification Code',
             html: emailHtml,
         };
 
@@ -111,7 +66,7 @@ export const sendConfirmationEmail = async (email, emailHtml, termsOfRegistratio
         const mailOptions = {
             from: process.env.EMAIL_USER,
             to: email,
-            subject: 'STREETFURY: Registration Successful!',
+            subject: 'MINUS01 STREETJAM: Registration Successful!',
             html: emailHtml,
             attachments: attachments,
         };
